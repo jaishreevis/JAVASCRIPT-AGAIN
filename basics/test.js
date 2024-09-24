e8e61801803cc3d28f4d1fd11805a00e380eab4f
Jaishree <jaishreevis@gmail.com>
2024-09-24 07:59:19 +0530

    test

--- basics/test.js
+++ basics/test.js
@@ -1,4 +1,4 @@
-// javascript is a dynamically typed programming language is one where you don't have to explicitly declare the type of a variable when you write your code. The type of the variable is determined at runtime, meaning when the program is actually running.
+// dynamically typed programming language - javascript is a dynamically typed programming language is one where you don't have to explicitly declare the type of a variable when you write your code. The type of the variable is determined at runtime, meaning when the program is actually running.
 
 console.log("hi jaishree")
 console.log("hi hey")
