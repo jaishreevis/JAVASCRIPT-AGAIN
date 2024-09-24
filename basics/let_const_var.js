878ac8fe3db7f40139cff2a9dee7919fb6a181b1
Jaishree <jaishreevis@gmail.com>
2024-09-24 07:57:48 +0530

    let_var_const

---
+++ basics/let_const_var.js
@@ -0,0 +1,37 @@
+// var let and const are variables .
+// variables arelike containers that are used to store data in the javascript.
+// javascript variables must have unique names. These names are called identifiers.
+
+// we can understand these variables by using block {} , more easily
+{
+  // we can not change the value of const variable
+  const name = "jaish"
+  console.log(name)
+}
+{
+  // we can re-assign the value of the variable
+  let age = 22;
+  // console.log(age)
+  age = 10 
+  console.log(age)
+}
+{
+// DO NOT USE THIS... they are not block scoped so not worthy of using.
+  var gender = "female"
+  console.log(gender)
+}
+{
+  // they are also var variables. Suggested not to use like this
+  email="jaishreevis"
+}
+// console.log(name) 
+// console.log(age)
+// console.log(gender)
+// console.log(email)
+
+const name = "jaish";
+let age = 22;
+var gender = "female";
+email="jaishreevis";
+let status;
+// console.table([name,age,gender,email,status])
--- basics/test.js
+++ basics/test.js
@@ -1,2 +1,4 @@
+// javascript is a dynamically typed programming language is one where you don't have to explicitly declare the type of a variable when you write your code. The type of the variable is determined at runtime, meaning when the program is actually running.
+
 console.log("hi jaishree")
 console.log("hi hey")
