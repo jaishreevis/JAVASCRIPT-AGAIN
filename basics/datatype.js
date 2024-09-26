3f12337422282ad5d5171a60c3d4a8c10b968318
Jaishree <jaishreevis@gmail.com>
2024-09-26 06:25:15 +0530

    datatype conversion

---
+++ basics/datatype.js
@@ -0,0 +1,38 @@
+"use strict"; //it prevents us to make mistakes or do unsafe actions like declaring a variable without a any keyword(let,const,var)and more
+
+// SSNNBB
+
+// String
+// Symbol
+// Number => 2 to power 53
+// null //(standalone value)
+// Boolean
+// BigInt
+// undefined
+// object
+
+// console.log(typeof undefined)
+// console.log(typeof null)
+
+//          DATATYPE CONVERSION CONFUSION
+// let score = "77aa"
+// console.log(typeof(score))
+// let newscore = Number(score)
+// console.log(typeof newscore)
+// console.log(newscore)
+
+// let bool = 0
+// console.log(typeof bool)
+// console.log(bool)
+
+// let newbool = Boolean(bool)
+// console.log(typeof newbool)
+// console.log(newbool)
+
+let val="heu"
+console.log(val)
+console.log(typeof val)
+
+let newval = Boolean(val)
+console.log(newval)
+console.log(typeof newval)
