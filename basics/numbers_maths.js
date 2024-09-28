46c5085d549b0f067918e8c5052b4e8dec8b4691
Jaishree <jaishreevis@gmail.com>
2024-09-29 04:26:49 +0530

    numbers and maths

---
+++ basics/numbers_maths.js
@@ -0,0 +1,48 @@
+// console.log("NUMBERS")
+// score = 100
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(typeof balance)
+
+// let newbal = balance.toString()
+// console.log(typeof newbal)
+// console.log(newbal.length)
+// console.log(typeof balance)
+
+/*toFixed is about how many numbers after the decimal, and toPrecision is about how many total numbers you want to see.*/
+// const newNumber =192.999994334
+// console.log(newNumber.toFixed(2));
+// console.log(typeof newNumber.toFixed(2));
+// console.log(newNumber.toPrecision(4));
+// console.log(typeof newNumber.toPrecision(3));
+
+// const localNumber= 100000000
+// console.log(localNumber.toLocaleString('en-IN'))
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.EPSILON)
+
+
+// ------------------------------------------------
+console.log('MATHS')
+// console.log(Math);
+// console.log(Math.abs(-3))
+// console.log(Math.round(4566462.8))
+// console.log(Math.ceil(4.8))
+// console.log(Math.floor(4.8))
+// console.log(Math.sqrt(16))
+// console.log(Math.pow(2,2))
+// console.log(Math.min(3,5,1,5,7))
+// console.log(Math.max(3,5,1,5,7))
+// console.log(Math.max(3,5,1,5,7))
+
+// console.log(Math.floor((Math.random()*10) + 1))
+
+let min = 20
+let max = 40
+
+console.log(Math.floor(Math.random() * (max-min + 1))+ min)
