1eb71c1e8d62c961a6642437f3237097cb7137fd
Jaishree <jaishreevis@gmail.com>
2024-09-29 07:33:24 +0530

    array methods

---
+++ program/array.js
@@ -0,0 +1,29 @@
+/*SHALLOW COPY AND DEEP COPY */
+/* shallow copy of an object is a copy whose properties share the same Reference as those of the source object from which the copy was made. in short if we change the value it changes all over the program like HEAP MEMORY*/
+/* deep copy of an object is a copy whose properties DO NOT share the same Reference as those of the source object from which the copy was made. like STACK MEMORY  */
+/* WHENEVER WE PERFORM COPY OPERATIONS IN ARRAY IT CREATES A SHALLOW COPY  */
+
+// let myNewArr = new Array(1,2,3,4)
+// console.log(myNewArr[1])
+
+// let myArr= [0,1,2,3,4]
+// console.log(myArr)
+// console.log(myArr.push(6))
+// console.log(myArr.push(9))
+// console.log(myArr)
+// console.log(myArr.pop())
+// console.log(myArr)
+
+// myArr.unshift(8)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(typeof myArr.includes(9))
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+// console.log(myArr.indexOf(9)) //-1 as it does not exist
+
+// const newArr = myArr.join("")
+// console.log(newArr)
+// console.log(typeof newArr)
+
