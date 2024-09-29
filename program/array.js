695d3ef2c364c614a64f0a5ed4fefd00e0b5b426
Jaishree <jaishreevis@gmail.com>
2024-09-30 02:29:53 +0530

    arrays

--- program/array.js
+++ program/array.js
@@ -28,11 +28,32 @@
 // console.log(typeof newArr)
 
 // SLICE SPICE
-let arr = [0,1,2,3,4,5,6]
-console.log(arr.slice(1,3))
-console.log("slice--------------",arr)
-console.log(arr.splice(1,3))
-console.log("splice-------------",arr)
+// let arr = [0,1,2,3,4,5,6]
+// console.log(arr.slice(1,3))
+// console.log("slice--------------",arr)
+// console.log(arr.splice(1,3))
+// console.log("splice-------------",arr)
 
 /* splice includes the range while slice dont and
 MAIN DIFF is slice does not change the  original arrya while splice manipulates the original array*/
+
+let array1 = [1,2,3,4]
+let array2=[5,6,7,8]
+let mainArraY = [...array1,...array2]
+console.log(mainArraY)
+let allarray = array1.concat(array2)
+console.log(allarray)
+
+let newarray = [1,2,3,[3,5,6,[4,6,3,5,7]]]
+console.log(newarray)
+let flatarray = newarray.flat(Infinity)
+console.log(flatarray)
+
+console.log(Array.isArray(newarray))
+console.log(Array.from("jaish"))
+console.log(Array.from({name:"hitesh"})) //Interview
+ 
+let num1 =100
+let num2 =200
+let num3 =300
+console.log(Array.of(num1,num2,num3))
