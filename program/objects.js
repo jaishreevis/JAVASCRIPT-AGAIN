d9590a8105616dae2265e892afb63074b35e9f86
Jaishree <jaishreevis@gmail.com>
2024-09-30 03:01:10 +0530

    objects

---
+++ program/objects.js
@@ -0,0 +1,33 @@
+// console.log("object")
+
+
+// //singleton-we create singleton array using object.create
+// // Object.create
+
+// const mySym = Symbol("sym1")// SYMBOL
+// // console.log(mySym)
+// //Object literals
+// let user ={
+//   name:"jaish",
+//   "age age":22,
+//   location:"delhi",
+//   email:"jaishreevis@gmail.com",
+//   isLoggedIn:false,
+//   [mySym]: "mykey1",
+// }
+// console.log(user.name)
+// // console.log(user.age )
+// // console.log(user[name])// this is wrong as name is a string
+// console.log(user["name"])
+// console.log(user["age age"])
+// console.log(user[mySym])
+
+// // if you want to freeze a object and dont want amyone to chnage any of its property use Object.freeze(___)
+// user.isLoggedIn=true
+// console.log(user)
+
+// Object.freeze(user)//does not changes anything in user object we defined.
+
+// user.isLoggedIn=false
+// console.log(user)
+
