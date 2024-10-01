164a25b8540d107241e6f94535299386db8e76f1
Jaishree <jaishreevis@gmail.com>
2024-10-02 02:29:49 +0530

    functions

---
+++ program/functions.js
@@ -0,0 +1,23 @@
+// console.log("functions")
+
+// function greet(){
+//   console.log("good morning")
+// }
+// greet();
+
+// function add(num1,num2){
+//   console.log(num1+num2)
+// }
+// add(2,3);
+
+// function sum(num1,num2){
+//   return(num1+num2)
+// }
+// sum(2,3);
+// const result = sum(5,4)
+// console.log(result)
+
+// function calculateprice(a, ...num){
+//   return (a,num)
+// }
+// console.log(calculateprice(200,300,500))
